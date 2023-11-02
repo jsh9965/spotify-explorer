31d15c528a0dc7ffb3430e497c24fd0d008d468d
jsh9965 <144354475+jsh9965@users.noreply.github.com>
2023-11-02 06:09:43 -0400

    Create db.mjs

---
+++ db.mjs
@@ -0,0 +1,30 @@
+// 1ST DRAFT DATA MODEL
+const mongoose = require('mongoose');
+
+// users
+const User = new mongoose.Schema({
+  // username provided by authentication plugin
+  // password hash provided by authentication plugin
+  lists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'songList' }],
+friends:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, {
+  _id: true
+});
+
+// Song
+//Each song is just a title and artist
+//Will only store songs which are featured in a least one user's playlsit
+const Song = new mongoose.Schema({
+  title: {type: String, required: true},
+  Artist: {type: String, required: true}
+});
+
+// Song list
+// * Can have any numebr of songs
+// can create automatically by applying restraints to existing playlsits
+const songList = new mongoose.Schema({
+  name: {type: String, required: true},
+  songs:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }]
+});
+
+// TODO: add remainder of setup for slugs, connection, registering models, etc. below
