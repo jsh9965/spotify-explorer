f4f245ae5ec5f0ec488230d2463d68fa418a2af8
jsh9965 <jsh9965@nyu.edu>
2023-11-02 06:47:32 -0400

    skeleton code

--- app.mjs
+++ app.mjs
@@ -6,4 +6,31 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    //redirect to myAccount if correct
+});
+
+app.get('/User/username', (req, res) => {
+    res.render('user');
+});
+
+app.get('/myAccount', (req, res) => {
+    res.render('myAccount');
+});
+
+app.get('/Editor', (req, res) => {
+    res.render('editor');
+});
+
+app.post('/Editor', (req, res) => {
+    //create playlist
+});
+
+
 app.listen(process.env.PORT || 3000);
