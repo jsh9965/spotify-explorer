a9e9e19c407d292035b18c1575178179b6efe97c
jsh9965 <jsh9965@nyu.edu>
2023-12-05 01:00:26 -0500

    can authenticate with spotify using passport

--- app.mjs
+++ app.mjs
@@ -7,6 +7,8 @@ import mongoose from 'mongoose';
 import url from 'url';
 //const Review = mongoose.model('Review');
 import session from 'express-session';
+import passport from 'passport';
+import { Strategy as SpotifyStrategy } from 'passport-spotify';
 
 const app = express();
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
@@ -20,35 +22,73 @@ app.use(express.urlencoded({ extended: false }));
 
 const sessionOptions = { 
 	secret: 'secret', 
-	saveUninitialized: false, 
-	resave: false 
+	saveUninitialized: true, 
+	resave: true 
 };
 app.use(session(sessionOptions));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new SpotifyStrategy(
+        {
+            clientID: '93f9f155c30949a98f9e0f3ee7aab905',
+            clientSecret: '340a2326cff24d74bbddfd1ea681f348',
+            callbackURL: 'http://localhost:24931/myAccount' // Adjust the URL based on your setup
+        },
+        function(accessToken, refreshToken, expires_in, profile, done) {
+
+            // Save the user profile information in the session
+            req.session.accessToken = accessToken
+            res.locals.accessToken = req.session.accessToken;
+            req.session.refreshToken = refreshToken
+            res.locals.refreshToken = req.session.refreshToken;
+            req.session.expires_in = expires_in
+            res.locals.expires_in = req.session.expires_in;
+            req.session.profile = profile
+            res.locals.profile = req.session.profile;
+            console.log("profile");
+            console.log(profile);
+            return done(null, profile);
+        }
+    )
+);
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
 
 
 
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.redirect('/auth/spotify');
 });
 
-app.post('/login', (req, res) => {
-    //redirect to myAccount if correct
-});
+app.get('/auth/error', (req, res) => res.send('Unknown Error'))
+app.get('/auth/spotify',passport.authenticate('spotify'));
 
 app.get('/User/username', (req, res) => {
     res.render('user');
 });
 
-app.get('/myAccount', (req, res) => {
+app.get('/myAccount', passport.authenticate('spotify', { failureRedirect: '/editor' }), async (req, res) => {
     if(!(req.session.pfp))
     {
         req.session.pfp = "/public/img/defaultPFP.avif"
         res.locals.pfp = req.session.pfp;
     }
+    console.log("req")
+    console.log(req.session)
+    console.log(req.user)
     res.render('myAccount', { username: req.session.username, pfp: req.session.pfp });
 });
 
-app.post('/myAccount', (req, res) => {
+app.post('/myAccount', async (req, res) => {
     if(req.body.username)
     {
         req.session.username = req.body.username;
